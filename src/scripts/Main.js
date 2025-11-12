62492699a4f7df1607431452d46f60b9f5649c3e
alexandrelajoie1 <alexlajoie4@gmail.com>
2025-11-12 12:26:42 -0500

    bientot fini..rendu au swiper

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,5 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
@@ -7,6 +8,15 @@ class Main {
 
   init() {
     Icons.load();
+    this.initPagination();
+  }
+
+  initPagination() {
+    const swiper = new Swiper('.js-swiper', {
+      pagination: {
+        el: '.swiper-paginaion',
+      },
+    });
   }
 }
 new Main();
